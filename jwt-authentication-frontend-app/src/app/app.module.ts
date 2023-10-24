469a118f46609d0e559c7e31ad52f5084865072e
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-25 00:53:55 +0530

    Updated AppModule with the new component 'events' inside the decorators property of the decorator 'NgModule'

--- jwt-authentication-frontend-app/src/app/app.module.ts
+++ jwt-authentication-frontend-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
