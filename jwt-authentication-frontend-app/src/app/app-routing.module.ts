e8d42d9467c043cd55b79d5a3e6ad3eac9dd379e
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-25 01:09:20 +0530

    Populated the 'routes' array in the app-routing.module with routes in the application and formatted this TS module using Prettier extension

--- jwt-authentication-frontend-app/src/app/app-routing.module.ts
+++ jwt-authentication-frontend-app/src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { EventsComponent } from './events/events.component';
+import { SpecialEventsComponent } from './special-events/special-events.component';
 
-const routes: Routes = [];
+// application routes
+const routes: Routes = [
+  {
+    path: '', // route for root path
+    redirectTo: '/events',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login', // route for login
+    component: LoginComponent,
+  },
+  {
+    path: 'register', // route for register
+    component: RegisterComponent,
+  },
+  {
+    path: 'events', // route for events
+    component: EventsComponent,
+  },
+  {
+    path: 'special', // route for special events
+    component: SpecialEventsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
