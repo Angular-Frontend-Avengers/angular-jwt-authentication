8d584988604ce50f320c573d8faf36bd1f9e64bb
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-25 00:56:37 +0530

    Created 'special-events' component to render and manage the special events particular to an authenticated user shown in the application

---
+++ jwt-authentication-frontend-app/src/app/special-events/special-events.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SpecialEventsComponent } from './special-events.component';
+
+describe('SpecialEventsComponent', () => {
+  let component: SpecialEventsComponent;
+  let fixture: ComponentFixture<SpecialEventsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SpecialEventsComponent]
+    });
+    fixture = TestBed.createComponent(SpecialEventsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ jwt-authentication-frontend-app/src/app/special-events/special-events.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-special-events',
+  templateUrl: './special-events.component.html',
+  styleUrls: ['./special-events.component.css']
+})
+export class SpecialEventsComponent {
+
+}
