56ef7233204258e59fc84d337be5833ad5870ff3
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-25 23:00:32 +0530

    Imported HttpClientModule in AppModule and added in the 'imports' attribute of the '@NgModule' decorator to communicate with backend over HTTP protocol

--- jwt-authentication-frontend-app/src/app/app.module.ts
+++ jwt-authentication-frontend-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './component/register/register.component';
 import { LoginComponent } from './login/login.component';
@@ -16,7 +17,7 @@ import { SpecialEventsComponent } from './special-events/special-events.componen
     EventsComponent,
     SpecialEventsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
