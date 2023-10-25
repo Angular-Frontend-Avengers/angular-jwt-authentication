fbe7e1b99caf998edf9e612bfb0af2b45d44fa56
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-25 21:19:10 +0530

    Implemented the execution of register button click event handler function inside RegisterComponent class and initialized the UserRegistrationData reference to capture the data submitted by user from registration form in RegisterComponent class

--- jwt-authentication-frontend-app/src/app/register/register.component.ts
+++ jwt-authentication-frontend-app/src/app/register/register.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
 
+import { UserRegistrationData } from './register.model';
+import RegisterEventHandler from './register.event_handler';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  // binded with user registration form in 'register.component.html'
+  protected registerUserData: UserRegistrationData;
+
+  private constructor() {
+    this.registerUserData = { email: '', password: '' };
+  }
+
+  // event handler: register button click in registration form
+  protected onRegisterButtonClick = (): void =>
+    RegisterEventHandler.onRegisterButtonClick(this.registerUserData);
+}
