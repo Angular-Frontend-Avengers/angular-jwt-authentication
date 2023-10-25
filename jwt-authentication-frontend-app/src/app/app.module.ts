bc5842b66deb22e79d4a778382439135fdce322c
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-25 22:41:13 +0530

    Created directory 'component' to store all the components under it and moved register component inside it

--- jwt-authentication-frontend-app/src/app/app.module.ts
+++ jwt-authentication-frontend-app/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './component/register/register.component';
 import { LoginComponent } from './login/login.component';
 import { EventsComponent } from './events/events.component';
 import { SpecialEventsComponent } from './special-events/special-events.component';
