18c462cdc080db17a1d559ab6ea132caff2fa3e5
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-26 14:24:11 +0530

    Injecting the HttpClient dependency through the constructor, updated the auth service register method by calling the register user API and returning the API response as an Observable

--- jwt-authentication-frontend-app/src/app/service/auth/auth.service.ts
+++ jwt-authentication-frontend-app/src/app/service/auth/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { UserRegistrationData } from 'src/app/component/register/register.model';
+
+import RouteConstants from '../routes';
+import { Observable } from 'rxjs';
+import { RegisterResponse } from 'src/app/api_response/register/RegisterResponse';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  registerUser(userRegistrationData: UserRegistrationData): Observable<Object> {
+    return this.httpClient.post(
+      `${RouteConstants.BACKEND_HOST}${RouteConstants.API_ROUTE}${RouteConstants.USER_REGISTRATION}`,
+      userRegistrationData
+    );
+  }
 }
