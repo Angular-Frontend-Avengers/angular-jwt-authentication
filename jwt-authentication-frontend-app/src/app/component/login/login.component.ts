0df933331141347cacb951d2b5c91e6555f98069
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-26 18:38:38 +0530

    Implemeted Login form view and created variable in the LoginComponent class for 2-way data-binding with the form elements in the Login component's template and defined the model for the same in the component's model TS-module

--- jwt-authentication-frontend-app/src/app/component/login/login.component.ts
+++ jwt-authentication-frontend-app/src/app/component/login/login.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { UserLoginData } from './login.model';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  protected userLoginData: UserLoginData;
+
+  constructor(private authService: AuthService) {
+    this.userLoginData = { email: '', password: '' };
+  }
 
 }
