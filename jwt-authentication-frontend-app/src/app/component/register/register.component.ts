bcdd55ae5c8537522e4c86af3bab8f1cc9c8724c
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-26 14:28:36 +0530

    Injecting AuthService dependency through the constructor, updated the click event handler method for register button in register form to call the register button click handler method from the event handler module for the register component and update the RegisterComponent class constructor as public (previously private)

--- jwt-authentication-frontend-app/src/app/component/register/register.component.ts
+++ jwt-authentication-frontend-app/src/app/component/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { UserRegistrationData } from './register.model';
 import RegisterEventHandler from './register.event_handler';
+import { AuthService } from 'src/app/service/auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -12,11 +13,14 @@ export class RegisterComponent {
   // binded with user registration form in 'register.component.html'
   protected registerUserData: UserRegistrationData;
 
-  private constructor() {
+  constructor(private authService: AuthService) {
     this.registerUserData = { email: '', password: '' };
   }
 
   // event handler: register button click in registration form
   protected onRegisterButtonClick = (): void =>
-    RegisterEventHandler.onRegisterButtonClick(this.registerUserData);
+    RegisterEventHandler.onRegisterButtonClick(
+      this.registerUserData,
+      this.authService
+    );
 }
