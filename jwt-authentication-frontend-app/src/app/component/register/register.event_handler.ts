d1d3202907f1935742ffef3aaf6914680e97bcca
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-26 14:20:17 +0530

    Updated the register click event handler method with call to the auth register service method and subscribed its Observable response using Observer instance (to consume the API response returned from the auth register service method) with its behavior defined in case of success and error response returned from the auth register service method

--- jwt-authentication-frontend-app/src/app/component/register/register.event_handler.ts
+++ jwt-authentication-frontend-app/src/app/component/register/register.event_handler.ts
@@ -1,10 +1,35 @@
+import { RegisterResponse } from 'src/app/api_response/register/RegisterResponse';
+import { AuthService } from 'src/app/service/auth/auth.service';
 import { UserRegistrationData } from './register.model';
+import { Observer } from 'rxjs';
 
 export default {
   // event handler: register button click in registration form
-  onRegisterButtonClick: (registerUserData: UserRegistrationData): void => {
+  onRegisterButtonClick: (
+    registerUserData: UserRegistrationData,
+    authService: AuthService
+  ): void => {
     console.log(
       `This is user registration data: ${JSON.stringify(registerUserData)}`
     );
+    const registerResponseObserver: Partial<Observer<Object>> = {
+      next(apiResponse) {
+        console.log(
+          `User registered successfully with details - ${JSON.stringify(
+            apiResponse
+          )}`
+        );
+        alert('User registered successfully!');
+      },
+      error(err: any) {
+        console.error(
+          `User failed to register with details - ${JSON.stringify(err)}`
+        );
+        alert('User registration failed');
+      },
+    };
+    authService
+      .registerUser(registerUserData)
+      .subscribe(registerResponseObserver);
   },
 };
