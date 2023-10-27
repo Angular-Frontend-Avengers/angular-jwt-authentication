45ad214842bd79382e813c057cca0658605e4ff3
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-27 21:44:50 +0530

    Having HttpClient dependency injected via constructor, implemented service method to fetch events in EventsService service

--- jwt-authentication-frontend-app/src/app/service/events/events.service.ts
+++ jwt-authentication-frontend-app/src/app/service/events/events.service.ts
@@ -1,9 +1,19 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import Event from 'src/app/api_response/events/event';
+import RouteConstants from '../routes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export default class EventsService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getEvents(): Observable<Object> {
+    return this.httpClient.get<Event>(
+      `${RouteConstants.BACKEND_HOST}${RouteConstants.API_ROUTE}${RouteConstants.EVENTS}`
+    );
+  }
 }
