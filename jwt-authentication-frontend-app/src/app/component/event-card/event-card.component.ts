5a3f8a0696a1934173436852be3608c55f663380
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-27 20:09:59 +0530

    Added prop fields of the event-card component in EventCardComponent class to receive these fields data from the parent component 'event' as props and display in the template of the 'event-card' component thereby render an event card in the 'events' and 'members' screen of the application

--- jwt-authentication-frontend-app/src/app/component/event-card/event-card.component.ts
+++ jwt-authentication-frontend-app/src/app/component/event-card/event-card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event-card',
   templateUrl: './event-card.component.html',
-  styleUrls: ['./event-card.component.css']
+  styleUrls: ['./event-card.component.css'],
 })
 export class EventCardComponent {
+  @Input() name: string;
+  @Input() description: string;
+  @Input() date: string;
+
+  constructor() {
+    this.name = '';
+    this.description = '';
+    this.date = '';
+  }
+
+  protected formatEventDate(timestamp: string) {
+    return new Date(timestamp).toLocaleDateString();
+  }
 
 }
