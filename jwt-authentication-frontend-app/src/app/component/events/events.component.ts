756446a596670ea0e51049cf9e645ec619270ba0
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-27 21:48:12 +0530

    Having inject EventsService via constructor, called the service method to fetch all the events and store it in a static field created to store the list of events and render in the 'event' component's template

--- jwt-authentication-frontend-app/src/app/component/events/events.component.ts
+++ jwt-authentication-frontend-app/src/app/component/events/events.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Observer } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+
+import { Events } from './events.model';
+import EventsService from '../../service/events/events.service';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.css']
+  styleUrls: ['./events.component.css'],
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit {
+  protected static events: Events[] = [];
 
+  constructor(private eventsService: EventsService) {}
+
+  // fetch events
+  ngOnInit(): void {
+    const eventsResponseObserver: Partial<Observer<Object>> = {
+      next(eventsAPIResponse) {
+        EventsComponent.events = eventsAPIResponse as Events[];
+      },
+      error(err) {
+        console.error(`Error in fetching the events: ${JSON.stringify(err)}`);
+      },
+    };
+    this.eventsService.getEvents().subscribe(eventsResponseObserver);
+  }
 }
