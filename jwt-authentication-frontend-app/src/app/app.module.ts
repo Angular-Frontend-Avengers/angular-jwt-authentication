0d8ed0521951ac6b1f2d6c08587b3372f3b3740d
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-27 20:05:42 +0530

    Created 'event-card' component for constructing a event cad in the application

--- jwt-authentication-frontend-app/src/app/app.module.ts
+++ jwt-authentication-frontend-app/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RegisterComponent } from './component/register/register.component';
 import { LoginComponent } from './component/login/login.component';
 import { EventsComponent } from './events/events.component';
 import { SpecialEventsComponent } from './special-events/special-events.component';
+import { EventCardComponent } from './component/event-card/event-card.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { SpecialEventsComponent } from './special-events/special-events.componen
     LoginComponent,
     EventsComponent,
     SpecialEventsComponent,
+    EventCardComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
